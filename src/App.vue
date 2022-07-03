2ef2b7b60f5f426c78ef97e6e8339b78aff30912
EvgeniyJet <mas-nira@mail.ru>
2022-07-03 10:37:52 +0700

    removed redundant files

--- src/App.vue
+++ src/App.vue
@@ -118,40 +118,73 @@ export default {
         let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight
 
         if (bottomOfWindow) {
-          // axios.get(`${PATH_BASE}${PATH_SEARCH}${PATH_USERS}?${PARAM_SEARCH}${this.query}&${PARAM_PAGE}${++this.page}`)
-          //     .then(response => response.data.items && (this.users = [...this.users, ...response.data.items]))
+          axios.get(`${PATH_BASE}${PATH_SEARCH}${PATH_USERS}?${PARAM_SEARCH}${this.query}&${PARAM_PAGE}${++this.page}`)
+              .then(response => response.data.items && (this.users = [...this.users, ...response.data.items]))
+
+							function getDataUser(){
+															// const gh_api = "https://api.github.com/search/users?q=";
+															// const userInput = username.value;
+															// const gh_endpoint = gh_api + userInput;
+															// const newUserArray = [];
+
+															// await axios
+															// .get(gh_endpoint)
+															// .then((res) => {
+															// 	const fetchRequest = res.data;
+
+															// 	return fetchRequest;
+															// })
+
+															// .then(async (res) => {
+															// 	console.log(res);
+															// 	for (let userIdx in res.items) {
+															// 		const userEP = "https://api.github.com/users/";
+																	
+															// 		const fetchRequest = await fetch(userEP + res.items[userIdx].login)
+															// 			.then((res) => {
+															// 				return res.json();
+															// 			})
+															// 			.then((res) => {
+															// 				return res;
+															// 			});           
+															// 	}
+															// 	users.value = res.items;					
+																
+															// });
+
+															// };
 					
-				const {data} = await axios.get(`${PATH_BASE}${PATH_SEARCH}${PATH_USERS}?${PARAM_SEARCH}${this.query}&${PATH_PER_PAGE}${DEFAULT_PER_PAGE}&${PARAM_PAGE}${this.page}`)
-        const {items, total_count} = data
-        this.userCount = total_count
+				// const {data} = await axios.get(`${PATH_BASE}${PATH_SEARCH}${PATH_USERS}?${PARAM_SEARCH}${this.query}&${PATH_PER_PAGE}${DEFAULT_PER_PAGE}&${PARAM_PAGE}${this.page}`)
+        // const {items, total_count} = data
+        // this.userCount = total_count
 
-        this.users = items
+        // this.users = items
 
-        if (items) {
-          await this.users.forEach(user => {
-            const {login, followers_url} = user
+        // if (items) {
+        //   await this.users.forEach(user => {
+        //     const {login, followers_url} = user
 
-            Promise.allSettled([
-              axios.get(`${PATH_BASE}${PATH_USERS}/${login}/repos?${PATH_PER_PAGE}100`),
-              axios.get(`${followers_url}?${PATH_PER_PAGE}100`)
-            ])
-                .then(results => {
-                  const [repos, followers] = results
+        //     Promise.allSettled([
+        //       axios.get(`${PATH_BASE}${PATH_USERS}/${login}/repos?${PATH_PER_PAGE}100`),
+        //       axios.get(`${followers_url}?${PATH_PER_PAGE}100`)
+        //     ])
+        //         .then(results => {
+        //           const [repos, followers] = results
 
-                  const status = 'fulfilled'
+        //           const status = 'fulfilled'
 
-                  if (repos.status === status && followers.status === status) {
-                    addDataInfoUser(user, {
-                      repo: repos.value.data,
-                      repo_count: repos.value.data.length,
-                      follower: followers.value.data,
-                      follower_count: followers.value.data.length
-                    })
-                  }
-                })
-                .catch(err => console.log(err))
-          })
-        }
+        //           if (repos.status === status && followers.status === status) {
+        //             addDataInfoUser(user, {
+        //               repo: repos.value.data,
+        //               repo_count: repos.value.data.length,
+        //               follower: followers.value.data,
+        //               follower_count: followers.value.data.length
+        //             })
+        //           }
+        //         })
+        //         .catch(err => console.log(err))
+        //   })
+        // }
         }
       }
     },
